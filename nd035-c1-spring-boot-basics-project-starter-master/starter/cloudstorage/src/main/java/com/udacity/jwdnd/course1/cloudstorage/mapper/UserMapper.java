b283a95a629676ca33b124a616397142f9255fbe
Luong Trung <trunghlbuh@gmail.com>
2023-09-24 22:12:36 +0700

    Update change

--- nd035-c1-spring-boot-basics-project-starter-master/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
+++ nd035-c1-spring-boot-basics-project-starter-master/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
@@ -1,9 +1,12 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
 
+@Mapper
 public interface UserMapper {
     @Select("SELECT * FROM USERS WHERE username = #{username}")
     User getUser(String username);
